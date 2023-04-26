070dfde34a5150047ca768101ec308657a94ec78
lyleskc <117544494+lyleskc@users.noreply.github.com>
2023-04-26 19:28:13 -0400

    Added clickable links and fixed navigation.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 console.log("Your index.js file is loaded correctly!");
+
+const links = document.querySelectorAll(".page-header ul a");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
